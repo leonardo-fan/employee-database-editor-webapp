cd74e375fa86485990bfd8e74ac34343292ee256
Leonardo Fan <leonardofan2000@gmail.com>
2022-02-03 22:46:32 +1100

    arrow func

--- server/server.js
+++ server/server.js
@@ -2,14 +2,14 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 5000; // either specified port on config.env or port 5000
+const port = process.env.PORT || 5000; // either specified port on config.env or port 5000 (both the same)
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/record"));
-const dbo = require("./db/conn") // db driver connection
+const dbo = require("./db/conn") // db driver connection, file in db folder
 
 app.listen(port, () => {
     // connect to db when server starts
-    dbo.connectToServer((err) => { if (err) console.error(err); });
+    dbo.connectToServer(err => { if (err) console.error(err); });
     console.log(`Server is running on port: ${port}`);
 });
