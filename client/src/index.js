1463c8ad92b5187aac45ebafdaa8ae2152635728
Leonardo Fan <leonardofan2000@gmail.com>
2022-02-04 17:32:07 +1100

    added bootstrap.css

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom"; // keeps UI transitions seamless, only reloads component that needs to be changed instead of the whole page
+import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.render(
     <React.StrictMode>
