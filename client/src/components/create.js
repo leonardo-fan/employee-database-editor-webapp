fdffa9673f76445b58d72ca619f5ff1ae687feb6
Leonardo Fan <leonardofan2000@gmail.com>
2022-02-04 13:24:13 +1100

    added create.js for adding employees

---
+++ client/src/components/create.js
@@ -0,0 +1,116 @@
+// create new records
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
+
+export default function Create() {
+    const [form, setForm] = useState({
+        name: "",
+        position: "",
+        level: ""
+    });
+    const navigate = useNavigate();
+
+    // set/update state
+    const updateForm = value => {
+        return setForm(prev => {
+            return { ...prev, ...value };
+        });
+    };
+
+    // submission handler
+    const handleSubmit = async e => {
+        e.preventDefault();
+
+        // when post request sent to create url, add new record
+        const newPerson = { ...form };
+
+        await fetch("http://localhost:5000/record/add", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newPerson),
+        })
+        .catch(error => {
+            window.alert(error);
+            return;
+        });
+
+        // reset form
+        setForm({ name: "", position: "", level: "" });
+        navigate("/");
+    };
+
+    return (
+        <div>
+            <h3>Create New Record</h3>
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label htmlFor="name">Name</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="name"
+                        value={form.name}
+                        onChange={(e) => updateForm({ name: e.target.value })}
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="position">Position</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="position"
+                        value={form.position}
+                        onChange={(e) => updateForm({ position: e.target.value })}
+                    />
+                </div>
+                <div className="form-group">
+                    <div className="form-check form-check-inline">
+                        <input
+                            className="form-check-input"
+                            type="radio"
+                            name="positionOptions"
+                            id="positionIntern"
+                            value="Intern"
+                            checked={form.level === "Intern"}
+                            onChange={(e) => updateForm({ level: e.target.value })}
+                        />
+                        <label htmlFor="positionIntern" className="form-check-label">Intern</label>
+                    </div>
+                    <div className="form-check form-check-inline">
+                        <input
+                            className="form-check-input"
+                            type="radio"
+                            name="positionOptions"
+                            id="positionJunior"
+                            value="Junior"
+                            checked={form.level === "Junior"}
+                            onChange={(e) => updateForm({ level: e.target.value })}
+                        />
+                        <label htmlFor="positionJunior" className="form-check-label">Junior</label>
+                    </div>
+                    <div className="form-check form-check-inline">
+                        <input
+                            className="form-check-input"
+                            type="radio"
+                            name="positionOptions"
+                            id="positionSenior"
+                            value="Senior"
+                            checked={form.level === "Senior"}
+                            onChange={(e) => updateForm({ level: e.target.value })}
+                        />
+                        <label htmlFor="positionSenior" className="form-check-label">Senior</label>
+                    </div>
+                </div>
+                <div className="form-group">
+                    <input
+                        type="submit"
+                        value="Add person"
+                        className="btn btn-primary"
+                    />
+                </div>
+            </form>
+        </div>
+    );
+}
