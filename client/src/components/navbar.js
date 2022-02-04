9d3748ee163bd3d9ad05a4b85f43e4bf20379ede
Leonardo Fan <leonardofan2000@gmail.com>
2022-02-04 17:31:18 +1100

    added emojis!

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,6 +1,5 @@
 // links to change the component viewed on the app
 import React from "react";
-import "bootstrap/dist/css/bootstrap.css";
 import { NavLink } from "react-router-dom";
 
 export default function Navbar () {
@@ -8,10 +7,7 @@ export default function Navbar () {
         <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <NavLink className="navbar-brand" to="/">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="30" stroke="dimgray" fill="dimgray" class="bi bi-house" viewBox="-5 1 16 16">
-                        <path fill-rule="evenodd" d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
-                        <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
-                    </svg>
+                    &#127968; Home
                 </NavLink>
                 <button
                     className="navbar-toggler"
@@ -29,7 +25,7 @@ export default function Navbar () {
                     <ul className="navbar-nav ml-auto">
                         <li className="nav-item">
                             <NavLink className="nav-link" to="/create">
-                                Create Record
+                                &#10133; Create New Record
                             </NavLink>
                         </li>
                     </ul>
