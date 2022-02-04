057c1d78b35c6e1a6fb622c51ff23f0ade449b24
Leonardo Fan <leonardofan2000@gmail.com>
2022-02-04 21:28:23 +1100

    styling and toggle button for smaller screens

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,33 +1,37 @@
 // links to change the component viewed on the app
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
+import "../../node_modules/bootstrap/js/src/collapse.js";
 
 export default function Navbar () {
+    const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+
+    const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+    
     return (
         <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <NavLink className="navbar-brand" to="/">
-                    &#127968; Home
-                </NavLink>
+            <nav className="navbar navbar-expand-lg sticky-top navbar-dark bg-dark">
+                <NavLink className="navbar-brand mx-3 my-1 h1" to="/">🏢 Employees</NavLink>
                 <button
-                    className="navbar-toggler"
+                    className="navbar-toggler mx-3"
                     type="button"
                     data-toggle="collapse"
                     data-target="#navbarSupportedContent"
                     aria-controls="navbarSupportedContent"
-                    aria-expanded="false"
+                    aria-expanded={!isNavCollapsed ? true : false}
                     aria-label="Toggle navigation"
+                    onClick={handleNavCollapse}
                 >
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav ml-auto">
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="/create">
-                                &#10133; Create New Record
-                            </NavLink>
-                        </li>
+                <div className={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`} id="navbarSupportedContent">
+                    <ul className="navbar-nav position-absolute end-0 mx-3">
+                            <li className="nav-item">
+                                <button className="btn btn-success">
+                                    <NavLink className="nav-link text-light" to="/create">➕ Create New Record</NavLink>
+                                </button>
+                            </li>
                     </ul>
                 </div>
             </nav>
