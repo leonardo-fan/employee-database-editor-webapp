c67407b1688a7909eb1c2bf8e3d2a57a247c2d14
Leonardo Fan <leonardofan2000@gmail.com>
2022-02-04 16:59:31 +1100

    added routes in export for App.js

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom"; // to define the application routes, changing which component is shown
+
+import Navbar from "./components/navbar";
+import RecordList from "./components/recordList";
+import Edit from "./components/edit";
+import Create from "./components/create";
+
+export default function App () {
+    // exact path means the url has to be an exact match to switch to the specified root - routes are default chosen with partial matches down the list
+    return (
+        <div>
+            <Navbar />
+            <Routes>
+                <Route exact path="/" element={<RecordList />} />
+                <Route path="/edit/:id" element={<Edit />} />
+                <Route path="/create" element={<Create />} />
+            </Routes>
+        </div>
+    );
+};
