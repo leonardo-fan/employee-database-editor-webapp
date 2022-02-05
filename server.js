b32421df58f57ad7382bbd55797968e51252ae2d
Leonardo Fan <leonardofan2000@gmail.com>
2022-02-05 14:50:14 +1100

    added azure app service functionality

--- server.js
+++ server.js
@@ -7,9 +7,18 @@ app.use(cors());
 app.use(express.json());
 app.use(require("./routes/record"));
 const dbo = require("./db/conn") // db driver connection, file in db folder
+const path = require("path"); 
 
 app.listen(port, () => {
     // connect to db when server starts
     dbo.connectToServer(err => { if (err) console.error(err); });
     console.log(`Server is running on port: ${port}`);
 });
+
+// display static react build if in production
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build")); 
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
