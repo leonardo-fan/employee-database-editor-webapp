8e89a914b07f12c400a738788775d5963e9eae9b
Leonardo Fan <leonardofan2000@gmail.com>
2022-02-06 03:05:36 +1100

    changed const port to app.set/app.get('port')

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path"); // for azure connection
 const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 5000; // either specified port on config.env or port 5000
+app.set('port', process.env.PORT || 5000); // either specified port on config.env or port 5000, cannot hardcode port for azure
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/record"));
@@ -17,8 +17,8 @@ if (process.env.NODE_ENV === "production") {
     });
 }
 
-app.listen(port, () => {
+app.listen(app.get('port'), () => {
     // connect to db when server starts
     dbo.connectToServer(err => { if (err) console.error(err); });
-    console.log(`Server is running on port: ${port}`);
+    console.log(`Server is running on port: ${app.get('port')}`);
 });
